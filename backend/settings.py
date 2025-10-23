989ed06f51e213dbb9ff69deac7b4676c2cf98f9
edmar-afk <jaywrsnp6@gmail.com>
2025-10-23 22:36:24 +0800

    new

--- backend/settings.py
+++ backend/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from pathlib import Path
 from datetime import timedelta
 import os
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -91,10 +92,9 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://<username>:<password>@<host>:<port>/<dbname>'
+    )
 }
 
 
