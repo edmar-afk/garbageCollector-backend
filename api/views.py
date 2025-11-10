53e0ad1e5df17e74b2ea857b8dd1a38bc5ead17a
edmar-afk <jaywrsnp6@gmail.com>
2025-11-10 09:43:10 +0800

    new

--- api/views.py
+++ api/views.py
@@ -48,7 +48,7 @@ class SendNotificationAPIView(views.APIView):
         )
 
         data = {
-            'sims': [330],
+            'sims': [390],
             'random_sender': False,
             'contact_lists': [],
             'mobile_numbers': [f'+63{phone_number[1:]}' if phone_number.startswith('0') else phone_number],
